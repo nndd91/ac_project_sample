726efff88e821b7c5f02c6d37b8cebd0f957c347
nndd91 <nndd91@hotmail.com>
2017-05-30 15:25:45 +0800

    Update valiation for joblisting

--- app/models/job_listing.rb
+++ app/models/job_listing.rb
@@ -1,5 +1,9 @@
 class JobListing < ApplicationRecord
 
   belongs_to :user
+  validates :user_id, numericality: true, presence: true
+  validates :position, presence: true
+  validates :salary, presence: true, numericality: true
+  validates :description, presence: true
 
 end
--- spec/models/job_listing_spec.rb
+++ spec/models/job_listing_spec.rb
@@ -4,5 +4,19 @@ RSpec.describe JobListing, type: :model do
 
   it { should belong_to(:user) }
 
-  
+  it do
+    should validate_presence_of(:user_id) 
+    should validate_numericality_of(:user_id)
+  end
+
+  it { should validate_presence_of(:position) }
+
+  it do
+     should validate_presence_of(:salary) 
+     should validate_numericality_of(:salary)
+
+  end
+
+  it { should validate_presence_of(:description) }
+
 end
